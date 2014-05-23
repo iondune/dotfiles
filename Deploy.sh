9fe4d3b0b67487602e9f19e756c5bb6a96f40409
Ian Dunn <iondune@gmail.com>
2014-05-22 21:55:35 -0700

    Update Deploy.sh and add .symlink extension to config files to support supdirectories

--- Deploy.sh
+++ Deploy.sh
@@ -1,6 +1,12 @@
-#! /bin/sh
+#! /bin/bash
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-for file in $(ls -1 $dir/config/) ; do
-    ln -sf "$dir/config/$file" "$HOME/.$file"
+cd $dir
+
+files=$(find ./config -name '*.symlink')
+for file in $files ; do
+    linkTarget="${file#./config/}"
+    linkSource="$HOME/.${linkTarget%.*}"
+    linkTarget="${dir}/config/${linkTarget}"
+    ln -sf "$linkTarget" "$linkSource"
 done
