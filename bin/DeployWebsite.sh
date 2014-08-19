3fe0e1837302107ae4fb16e31cbcb46c150b358d
Ian Dunn <iondune@gmail.com>
2014-08-19 20:39:49 +0000

    Update website deployment script for new jekyll build

--- bin/DeployWebsite.sh
+++ bin/DeployWebsite.sh
@@ -1,11 +1,9 @@
 echo "Pulling changes from repository..."
 echo "----------------------------------"
-hg -R ~/Projects/ionWebsite/ pull -u
+hg -R ~/Projects/ionJekyll/ pull -u
 echo
 echo "Synchronizing changes with webserver..."
 echo "---------------------------------------"
-rsync -a -e 'ssh -p 1157' --exclude=.hg --progress ~/Projects/ionWebsite/ iondunec@iondune.com:~/public_html/home
-echo
-echo "Applying file permissions..."
-ssh -p 1157 iondunec@iondune.com 'find public_html/home/ -type d | xargs chmod 755'
-ssh -p 1157 iondunec@iondune.com 'find public_html/home/ -type f | xargs chmod 644'
+cd ~/Projects/ionJekyll/
+jekyll build
+rsync -a _site/ /var/www/iondune.com/public_html/
