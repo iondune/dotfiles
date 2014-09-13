3ba0b73eff99233c05443572a9634d32e889110f
Ian Dunn <iondune@gmail.com>
2014-09-12 23:01:06 -0700

    Remove website deployment script, belongs in ionJekyll repo

--- bin/DeployWebsite.sh
+++
@@ -1,9 +0,0 @@
-echo "Pulling changes from repository..."
-echo "----------------------------------"
-hg -R ~/Projects/ionJekyll/ pull -u
-echo
-echo "Synchronizing changes with webserver..."
-echo "---------------------------------------"
-cd ~/Projects/ionJekyll/
-jekyll build
-rsync -a _site/ /var/www/iondune.com/public_html/
